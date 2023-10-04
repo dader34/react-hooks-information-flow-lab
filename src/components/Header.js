2a243399e07101f3729d026c090eeee810a725d0
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-03 20:08:43 -0700

    Passed alltests

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = ({isDarkMode,onDarkModeClick}) =>{
+    return(
+        <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {isDarkMode ? "Light" : "Dark"} Mode
+        </button>
+      </header>
+    )
+}
+
+export default Header;
